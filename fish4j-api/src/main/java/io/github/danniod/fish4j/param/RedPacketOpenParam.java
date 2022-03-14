21f0d1753c1d370b32f079e162ac7af8f15574d8
dannio <lendoon@gmail.com>
2022-03-14 09:56:35 +0800

    fixed

--- fish4j-api/src/main/java/io/github/danniod/fish4j/param/RedPacketOpenParam.java
+++ fish4j-api/src/main/java/io/github/danniod/fish4j/param/RedPacketOpenParam.java
@@ -1,5 +1,6 @@
 package io.github.danniod.fish4j.param;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -9,5 +10,6 @@ import io.github.danniod.fish4j.param.auth.BaseParam;
 @EqualsAndHashCode(callSuper = true)
 public @Data class RedPacketOpenParam extends BaseParam {
 
+    @JsonProperty("oId")
     private Long oId;
 }
